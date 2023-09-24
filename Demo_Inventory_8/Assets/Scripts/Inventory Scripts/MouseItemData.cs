d696c75a0e97fd84af160204a173f5fafcd7b99d
ArthurNguyen1 <ArthurNguyen0368@gmail.com>
2023-09-24 20:20:31 +0700

    Add empty project

---
+++ Demo_Inventory_8/Assets/Scripts/Inventory Scripts/MouseItemData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MouseItemData : MonoBehaviour
+{
+    public Image ItemSprite;
+    public TextMeshProUGUI ItemCount;
+}
