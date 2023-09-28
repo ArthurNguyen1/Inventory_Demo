e93a036074ec815c78ad30c10d9a9ef844d1878d
ArthurNguyen1 <ArthurNguyen0368@gmail.com>
2023-09-28 14:22:00 +0700

    add inventorySlot UI interaction code

--- Demo_Inventory_8/Assets/Scripts/Inventory Scripts/MouseItemData.cs
+++ Demo_Inventory_8/Assets/Scripts/Inventory Scripts/MouseItemData.cs
@@ -8,4 +8,10 @@ public class MouseItemData : MonoBehaviour
 {
     public Image ItemSprite;
     public TextMeshProUGUI ItemCount;
+
+    private void Awake()
+    {
+        ItemSprite.color = Color.clear;
+        ItemCount.text = "";
+    }
 }
